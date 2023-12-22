93440f8dff4964422e08dd08de028e426e5b933a
Hilmi <hilmi@gementar.com>
2023-12-22 23:16:02 +0800

    responsive query

---
+++ app/test/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from 'react'
+import { useMediaQuery } from 'usehooks-ts'
+import { breakpoints } from '../config/breakpoints'
+
+const page = () => {
+
+    const isMobile = useMediaQuery(`(max-width: ${breakpoints.mobile})`)
+
+
+    return (
+        <div>You are on mobile? = {isMobile ? 'yes' : 'no'}</div>
+    )
+}
+
+export default page
