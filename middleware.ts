ee5f450bfb2039fdf93652b50af685a4906aa4e2
Hilmi <119261516+themuhdhilmi@users.noreply.github.com>
2024-01-03 12:17:53 +0800

    Commit Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,3 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import prisma from './prisma/client';
- 
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+export { default } from "next-auth/middleware"
 
-  const token = request.cookies.get('next-auth.session-token')?.value;
-
-  const user = await prisma.user.findUnique(
-    {
-        where: {
-            id: token
-        },
-    }
-  )
-
-  return NextResponse.redirect(new URL('/lol' + (JSON.stringify(user)), request.url));
-}
- 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: '/panel',
-}
+export const config = { matcher: ["/panel/:path*"] }
